530febd3e683f337b5a352b419671b141c327de3
Amir Ahmadi <eamahma@gmail.com>
2021-12-31 21:58:01 -0500

    50.UserList Component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import UserResults from '../Users/UserResults'
+
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-    </div>
+    <>
+    {/* Search Componnet go here */}
+      <UserResults />
+    </>
   )
 }
 
